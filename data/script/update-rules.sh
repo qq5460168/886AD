a4e71c9f1bc53269b1a4c716e49c9a8be4da8684
Lwb828 <164605151+Lwb828@users.noreply.github.com>
2024-04-21 13:01:11 +0800

    Initial commit

---
+++ data/script/update-rules.sh
@@ -0,0 +1,154 @@
+LC_ALL='C'
+
+rm *.txt
+
+wait
+echo '创建临时文件夹'
+mkdir -p ./tmp/
+
+#添加补充规则
+#cp ./data/rules/adblock.txt ./tmp/rules01.txt
+#cp ./data/rules/whitelist.txt ./tmp/allow01.txt
+
+cd tmp
+
+echo '下载规则'
+rules=(
+  "https://ghproxy.net/https://raw.githubusercontent.com/qq5460168/-/main/black.txt"
+  "https://lingeringsound.github.io/adblock_auto/Rules/adblock_auto.txt"
+  "https://cdn.jsdelivr.net/gh/damengzhu/abpmerge@main/abpmerge.txt"
+ 
+  "https://gitlab.com/hagezi/mirror/-/raw/main/dns-blocklists/adblock/pro.plus.txt"
+  "https://raw.hellogithub.com/hosts"
+  "https://gitee.com/xinggsf/Adblock-Rule/raw/master/mv.txt"
+
+"https://raw.githubusercontent.com/qq5460168/-/main/white.txt"
+
+""
+ )
+
+allow=(
+  "https://raw.githubusercontent.com/qq5460168/-/main/white.txt"
+#anti白名单
+＂https://raw.githubusercontent.com/privacy-protection-tools/dead-horse/master/anti-ad-white-list.txt＂
+  
+)
+
+for i in "${!rules[@]}" "${!allow[@]}"
+do
+  curl -m 60 --retry-delay 2 --retry 5 --parallel --parallel-immediate -k -L -C - -o "rules${i}.txt" --connect-timeout 60 -s "${rules[$i]}" |iconv -t utf-8 &
+  curl -m 60 --retry-delay 2 --retry 5 --parallel --parallel-immediate -k -L -C - -o "allow${i}.txt" --connect-timeout 60 -s "${allow[$i]}" |iconv -t utf-8 &
+done
+wait
+echo '规则下载完成'
+
+# 添加空格
+file="$(ls|sort -u)"
+for i in $file; do
+  echo -e '\n' >> $i &
+done
+wait
+
+echo '处理规则中'
+
+cat | sort -n| grep -v -E "^((#.*)|(\s*))$" \
+ | grep -v -E "^[0-9f\.:]+\s+(ip6\-)|(localhost|local|loopback)$" \
+ | grep -Ev "local.*\.local.*$" \
+ | sed s/127.0.0.1/0.0.0.0/g | sed s/::/0.0.0.0/g |grep '0.0.0.0' |grep -Ev '.0.0.0.0 ' | sort \
+ |uniq >base-src-hosts.txt &
+wait
+cat base-src-hosts.txt | grep -Ev '#|\$|@|!|/|\\|\*'\
+ | grep -v -E "^((#.*)|(\s*))$" \
+ | grep -v -E "^[0-9f\.:]+\s+(ip6\-)|(localhost|loopback)$" \
+ | sed 's/127.0.0.1 //' | sed 's/0.0.0.0 //' \
+ | sed "s/^/||&/g" |sed "s/$/&^/g"| sed '/^$/d' \
+ | grep -v '^#' \
+ | sort -n | uniq | awk '!a[$0]++' \
+ | grep -E "^((\|\|)\S+\^)" & #Hosts规则转ABP规则
+
+cat | sed '/^$/d' | grep -v '#' \
+ | sed "s/^/@@||&/g" | sed "s/$/&^/g"  \
+ | sort -n | uniq | awk '!a[$0]++' & #将允许域名转换为ABP规则
+
+cat | sed '/^$/d' | grep -v "#" \
+ |sed "s/^/@@||&/g" | sed "s/$/&^/g" | sort -n \
+ | uniq | awk '!a[$0]++' & #将允许域名转换为ABP规则
+
+cat | sed '/^$/d' | grep -v "#" \
+ |sed "s/^/0.0.0.0 &/g" | sort -n \
+ | uniq | awk '!a[$0]++' & #将允许域名转换为ABP规则
+
+cat *.txt | sed '/^$/d' \
+ |grep -E "^\/[a-z]([a-z]|\.)*\.$" \
+ |sort -u > l.txt &
+
+cat \
+ | sed "s/^/||&/g" | sed "s/$/&^/g" &
+
+cat \
+ | sed "s/^/0.0.0.0 &/g" &
+
+
+echo 开始合并
+
+cat rules*.txt \
+ |grep -Ev "^((\!)|(\[)).*" \
+ | sort -n | uniq | awk '!a[$0]++' > tmp-rules.txt & #处理AdGuard的规则
+
+cat \
+ | grep -E "^[(\@\@)|(\|\|)][^\/\^]+\^$" \
+ | grep -Ev "([0-9]{1,3}.){3}[0-9]{1,3}" \
+ | sort | uniq > ll.txt &
+wait
+
+cat l*.txt \
+ |grep -v '^!' | grep -E -v "^[\.||]+[com]+[\^]$" \
+ |grep -Ev "^\^" \
+ |sort -n |uniq >> tmp1-dns1.txt & #处理DNS规则
+wait
+cat tmp1-dns1.txt \
+ | sort -n |uniq -u #去重过期域名
+wait
+
+cat *.txt | grep '^@' \
+ | sort -n | uniq > tmp-allow.txt & #允许清单处理
+wait
+
+echo 规则合并完成
+
+#移动规则到Pre目录
+cd ../
+mkdir -p ./pre/
+cp ./tmp/tmp-*.txt ./pre
+cd ./pre
+
+# Python 处理重复规则
+python .././data/python/rule.py
+
+# Start Add title and date
+diffFile="$(ls|sort -u)"
+for i in $diffFile; do
+ n=`cat $i | wc -l` 
+ echo "! Version: $(TZ=UTC-8 date +'%Y-%m-%d %H:%M:%S')（北京时间） " >> tpdate.txt 
+ new=$(echo "$i" |sed 's/tmp-//g') 
+ echo "! Total count: $n" > $i-tpdate.txt 
+ cat ./tpdate.txt ./$i-tpdate.txt ./$i > ./$new 
+ rm $i *tpdate.txt 
+done
+
+echo '更新统计数据'
+
+cd ../
+
+diffFile="$(ls pre |sort -u)"
+for i in $diffFile; do
+ titleName=$(echo "$i" |sed 's#.txt#-title.txt#') 
+ cat ./data/title/$titleName ./pre/$i | awk '!a[$0]++'> ./$i 
+ sed -i '/^$/d' $i 
+
+done
+wait
+echo '更新成功'
+rm -rf pre
+
+exit
