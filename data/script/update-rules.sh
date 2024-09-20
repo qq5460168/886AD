a20ea3f608edb0732a577afbceb81ff2a8de7d41
qq5460168 <109363085+qq5460168@users.noreply.github.com>
2024-09-20 19:15:30 +0800

    更新 update-rules.sh

--- data/script/update-rules.sh
+++ data/script/update-rules.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 LC_ALL='C'
 
 rm *.txt
@@ -11,30 +12,59 @@ cp ./data/rules/adblock.txt ./tmp/rules01.txt
 cp ./data/rules/whitelist.txt ./tmp/allow01.txt
 
 cd tmp
+#下载yhosts规则
+#curl https://raw.githubusercontent.com/VeleSila/yhosts/master/hosts | sed '/0.0.0.0 /!d; /#/d; s/0.0.0.0 /||/; s/$/\^/' > rules001.txt
+
+#下载大圣净化规则
+#curl https://raw.githubusercontent.com/jdlingyu/ad-wars/master/hosts > rules002.txt
+#sed -i '/视频/d;/奇艺/d;/微信/d;/localhost/d' rules002.txt
+#sed -i '/127.0.0.1 /!d; s/127\.0\.0\.1 /||/; s/$/\^/' rules002.txt
+
+#下载乘风视频过滤规则
+#curl https://raw.githubusercontent.com/xinggsf/Adblock-Plus-Rule/master/mv.txt | awk '!/^$/{if($0 !~ /[#^|\/\*\]\[\!]/){print "||"$0"^"} else if($0 ~ /[#\$|@]/){print $0}}' | sort -u > rules003.txt
+
 
 echo '下载规则'
 rules=(
-  "https://hub.gitmirror.com/https://raw.githubusercontent.com/qq5460168/-/main/black.txt" #5460
-  "https://ghproxy.net/https://raw.githubusercontent.com/damengzhu/banad/main/jiekouAD.txt" #大萌主
-  "https://ghproxy.net/https://raw.githubusercontent.com/afwfv/DD-AD/main/rule/DD-AD.txt"  #DD
+  "https://raw.githubusercontent.com/qq5460168/dangchu/main/black.txt" #5460
+  "https://raw.githubusercontent.com/damengzhu/banad/main/jiekouAD.txt" #大萌主
+  "https://raw.githubusercontent.com/afwfv/DD-AD/main/rule/DD-AD.txt"  #DD
   "https://raw.gitmirror.com/Cats-Team/dns-filter/main/abp.txt" #AdRules DNS Filter
-  "https://raw.hellogithub.com/hosts" #GitHub加速
-  "https://raw.githubusercontent.com/qq5460168/dangchu/main/0608.txt" #测试hosts
-  "https://raw.githubusercontent.com/qq5460168/-/main/white.txt" #白名单
+
+"https://raw.hellogithub.com/hosts" #GitHub加速
+  "https://raw.githubusercontent.com/qq5460168/dangchu/main/adhosts.txt" #测试hosts
+  "https://raw.githubusercontent.com/qq5460168/dangchu/main/white.txt" #白名单
   "https://raw.githubusercontent.com/loveqqzj/AdGuard/master/Mobile.txt" #loveqqzj
   "https://raw.githubusercontent.com/mphin/AdGuardHomeRules/main/Blacklist.txt" #mphin
-  "" #
-  "" #
+  "https://gitee.com/zjqz/ad-guard-home-dns/raw/master/black-list" #周木木
+  "https://raw.githubusercontent.com/liwenjie119/adg-rules/master/black.txt" #liwenjie119
+   #"https://raw.githubusercontent.com/qq5460168/dangchu/main/T%E7%99%BD%E5%90%8D%E5%8D%95.txt" #T白
+
+"" #
+  "https://github.com/entr0pia/fcm-hosts/raw/fcm/fcm-hosts" #FCM Hosts
+
+"https://raw.gitcode.com/rssv/qy-Ads-Rule/raw/main/black.txt" #晴雅
+
+"" #
+"" #
  
   
   "https://raw.githubusercontent.com/TG-Twilight/AWAvenue-Ads-Rule/main/AWAvenue-Ads-Rule.txt" #秋风规则
-  
-  
  )
 
 allow=(
-  "https://raw.githubusercontent.com/qq5460168/dangchu/main/white.txt"
-
+ "https://raw.githubusercontent.com/qq5460168/dangchu/main/white.txt" #
+  "https://raw.githubusercontent.com/mphin/AdGuardHomeRules/main/Allowlist.txt"
+  "https://file-git.trli.club/file-hosts/allow/Domains"#冷漠
+   "https://hub.gitmirror.com/https://raw.githubusercontent.com/user001235/112/main/white.txt" #浅笑
+  "" #
+  #"https://mirror.ghproxy.com/raw.githubusercontent.com/8680/GOODBYEADS/master/allow.txt" #8680
+  "https://gitee.com/zjqz/ad-guard-home-dns/raw/master/white-list" #周木木
+  "https://raw.githubusercontent.com/liwenjie119/adg-rules/master/white.txt" #liwenjie119
+  "https://raw.githubusercontent.com/qq5460168/dangchu/main/T%E7%99%BD%E5%90%8D%E5%8D%95.txt" #T白名单
+  "" #
+ 
+ 
 )
 
 for i in "${!rules[@]}" "${!allow[@]}"
@@ -104,54 +134,25 @@ cat \
  | sort | uniq > ll.txt &
 wait
 
-cat l*.txt \
- |grep -v '^!' | grep -E -v "^[\.||]+[com]+[\^]$" \
- |grep -Ev "^\^" \
- |sort -n |uniq >> tmp1-dns1.txt & #处理DNS规则
-wait
-cat tmp1-dns1.txt \
- | sort -n |uniq -u #去重过期域名
-wait
 
 cat *.txt | grep '^@' \
  | sort -n | uniq > tmp-allow.txt & #允许清单处理
 wait
 
-echo 规则合并完成
+cp tmp-allow.txt .././allow.txt
+cp tmp-rules.txt .././rules.txt
 
-#移动规则到Pre目录
-cd ../
-mkdir -p ./pre/
-cp ./tmp/tmp-*.txt ./pre
-cd ./pre
+echo 规则合并完成
 
 # Python 处理重复规则
 python .././data/python/rule.py
+python .././data/python/filter-dns.py
 
 # Start Add title and date
-diffFile="$(ls|sort -u)"
-for i in $diffFile; do
- n=`cat $i | wc -l` 
- echo "! Version: $(TZ=UTC-8 date +'%Y-%m-%d %H:%M:%S')（北京时间） " >> tpdate.txt 
- new=$(echo "$i" |sed 's/tmp-//g') 
- echo "! Total count: $n" > $i-tpdate.txt 
- cat ./tpdate.txt ./$i-tpdate.txt ./$i > ./$new 
- rm $i *tpdate.txt 
-done
+python .././data/python/title.py
 
-echo '更新统计数据'
 
-cd ../
-
-diffFile="$(ls pre |sort -u)"
-for i in $diffFile; do
- titleName=$(echo "$i" |sed 's#.txt#-title.txt#') 
- cat ./data/title/$titleName ./pre/$i | awk '!a[$0]++'> ./$i 
- sed -i '/^$/d' $i 
-
-done
 wait
 echo '更新成功'
-rm -rf pre
 
 exit
