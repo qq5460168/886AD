8756be354e024577aff67b5af3c3ca3df53c8586
qq5460168 <109363085+qq5460168@users.noreply.github.com>
2024-11-12 23:13:45 +0800

    更新 title.py

--- data/python/title.py
+++ data/python/title.py
@@ -20,9 +20,9 @@ for file_path in file_list:
 
     # 在文件顶部插入内容
     new_content = f"[Adblock Plus 2.0]\n" \
-                  f"! Title: GOODBYEADS\n" \
-                  f"! Homepage: https://github.com/8680/GOODBYEADS\n" \
-                  f"! Expires: 12 Hours\n" \
+                  f"! Title:去广告规则，酷安反馈反馈\n" \
+                  f"! Homepage: https://github.com/qq5460168/886AD\n" \
+                  f"! Expires: 2 Hours\n" \
                   f"! Version: {beijing_time}（北京时间）\n" \
                   f"! Description: 适用于AdGuard的去广告规则，合并优质上游规则并去重整理排列\n" \
                   f"! Total count: {line_count}\n" \
