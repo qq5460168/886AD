a54bba631b1fccc6202f3642261547274c26f534
qq5460168 <109363085+qq5460168@users.noreply.github.com>
2024-11-21 18:10:25 +0800

    Update qx.py

--- data/python/qx.py
+++ data/python/qx.py
@@ -13,6 +13,21 @@ def replace_content_in_file(input_file, output_file):
                     line = line.replace("||", "DOMAIN,").replace("^", ",reject")
                 file.write(line)
 
+def remove_whitelist_domains(input_file, whitelist_file):
+    with open(whitelist_file, 'r') as whitelist:
+        whitelist_domains = [entry.strip()[4:-1] for entry in whitelist if entry.strip().startswith('@@||') and entry.strip().endswith('^')]
+
+    with open(input_file, 'r') as file:
+        lines = file.readlines()
+
+    with open(input_file, 'w') as file:
+        for line in lines:
+            if not any(domain in line for domain in whitelist_domains):
+                file.write(line)
+
 input_file_path = ".././data/rules/dns.txt"
 output_file_path = ".././data/rules/qx.list"
+whitelist_file_path = ".././data/mod/whitelist.txt"
+
 replace_content_in_file(input_file_path, output_file_path)
+remove_whitelist_domains(output_file_path, whitelist_file_path)
